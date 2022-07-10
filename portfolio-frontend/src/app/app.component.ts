bb0a9bbc40c8795e3141667e4c6ee82872bc2ef1
mariquenaallosa <allosamariquena@gmail.com>
2022-07-10 18:08:29 -0300

    Update app.component.ts

--- portfolio-frontend/src/app/app.component.ts
+++ portfolio-frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 // Import ng-circle-progress
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { PersonaService } from './service/persona.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'PrInt_MA';
