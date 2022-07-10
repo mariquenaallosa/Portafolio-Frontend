156623d6e053dd7c78665c2c38d250c468b456a3
mariquenaallosa <allosamariquena@gmail.com>
2022-07-10 18:18:31 -0300

    Ruteo

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 //Conectar back con el front
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -29,7 +31,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     SkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
