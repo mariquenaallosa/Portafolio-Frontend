47d60814620f9bf8e6ff907890ea7fda6f944083
Mariquena <allosamariquena@gmail.com>
2023-08-11 00:32:17 -0300

    modificar rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 // Router
 const routes: Routes = [
-  {path:'', component: HomeComponent},
+  {path:'inicio', component: HomeComponent},
+  {path:'proyectos', component: ProyectosComponent},
+  {path:'resumen', component: AcercaDeComponent},
   {path:'login', component: LoginComponent}
 
   
